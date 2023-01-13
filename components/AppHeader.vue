b0c63b689682fb46bd6a090526ac1ee9ca433139
Astborg <josefinastborg@hotmail.com>
2023-01-13 20:03:02 +0100

    parent

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,168 +1,161 @@
 <template>
-<div>
+  <div>
     <div class="corner">
-    <picture>
-        <img src="~assets/litestörre.jpg">
-    </picture>
+      <picture>
+        <img src="~assets/litestörre.jpg" />
+      </picture>
+      <div>
+        <font-awesome-icon id="cart" :icon="['fas', 'cart-shopping']" />
+        <parent><div class="count">{{ count }}</div></parent>
+      </div>
+    </div>
     <div>
-    <font-awesome-icon id="cart" :icon="['fas', 'cart-shopping']"/>
-    <div class="count"> {{ count }} </div> 
+      <font-awesome-icon class="height" id="bar" :icon="['fas', 'bars']" />
     </div>
+    <div>
+      <ul class="menu hidden" id="menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Cashout</li>
+      </ul>
     </div>
-    <div><font-awesome-icon class="height" id="bar" :icon="['fas', 'bars']"/></div>
-    <div> 
-        <ul class="menu hidden" id="menu" >
-            <li>Home</li>
-            <li>About</li>
-            <li>Cashout</li>
-        </ul>
-    </div>
-</div>
+  </div>
 </template>
 <script setup>
-import {parent} from '../pages/index.vue' 
-
+import { parent } from '../pages/index.vue'
 </script>
 <style scoped>
-img{
-    width: 7vw;
-    border-radius: 50%; 
-    margin-top: 2vw;
-    margin-left: 2vw; 
-    transition-duration: 0.5s; 
-    justify-content: left;  
+img {
+  width: 7vw;
+  border-radius: 50%;
+  margin-top: 2vw;
+  margin-left: 2vw;
+  transition-duration: 0.5s;
+  justify-content: left;
 }
-img:hover{
-    transform: rotate(360deg)
+img:hover {
+  transform: rotate(360deg);
 }
-ul{
-    display: block;
+ul {
+  display: block;
 }
-.menu{
-    display: flex; 
-    justify-content: space-around;
+.menu {
+  display: flex;
+  justify-content: space-around;
 }
-.fa-bars{
-    display: none; 
-    cursor:pointer; 
+.fa-bars {
+  display: none;
+  cursor: pointer;
 }
 #cart {
-    position: absolute;
-    font-size: 2vw; 
-    margin-top: 5vw; 
-    margin-right: 1vw; 
-    width: 4vw; 
-    cursor:pointer; 
-    color: black; 
+  position: absolute;
+  font-size: 2vw;
+  margin-top: 5vw;
+  margin-right: 1vw;
+  width: 4vw;
+  cursor: pointer;
+  color: black;
 }
-#cart:active{
-    transform: scale(0.9)
+#cart:active {
+  transform: scale(0.9);
 }
-.count{
-    position: relative; 
-    background-color: orangered;
-    width: 1vw; 
-    height: 1vw; 
-    border-radius: 50%;
-    text-align: center; 
-    padding-top: 0.2vw;
-    padding-right: 0.1vw;  
-    padding-left: 0.1vw; 
-    font-weight: bold;
-    cursor: pointer;
-    margin-top: 6vw; 
-    margin-left: 2vw;
-    opacity: 0.8 
+.count {
+  position: relative;
+  background-color: orangered;
+  width: 1vw;
+  height: 1vw;
+  border-radius: 50%;
+  text-align: center;
+  padding-top: 0.2vw;
+  padding-right: 0.1vw;
+  padding-left: 0.1vw;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 6vw;
+  margin-left: 2vw;
+  opacity: 0.8;
+}
 
+.corner {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 63vw;
 }
-
-.corner{
+.height {
+  display: none;
+}
+li {
+  text-align: center;
+  list-style: none;
+  padding: 0.8vw;
+  cursor: pointer;
+}
+li:hover {
+  background-color: orangered;
+  border-radius: 15px;
+  transform: scale(1.2);
+  box-shadow: 1px 1px 1px 1px rgba(244, 172, 148, 0.776);
+}
+li:active {
+  transform: scale(0.98);
+}
+.change {
+  display: absolute;
+  float: right;
+  border-radius: 15px;
+  color: black;
+  margin-left: 10vw;
+  cursor: pointer;
+  padding: 0.8vw;
+}
+.change:hover {
+  font-weight: bold;
+  background-color: grey;
+  border-radius: 1.5vw;
+}
+@media screen and (max-width: 768px) {
+  .height {
+    height: 6vw;
     display: flex;
-    flex-wrap: wrap;
-    gap: 63vw
-
-}
-.height{
-    display: none; 
-}
-li{  
-   text-align: center;
-   list-style: none;
-   padding: 0.8vw;
-   cursor: pointer; 
- 
-}
-li:hover{
-    background-color: orangered; 
-    border-radius: 15px;
-    transform: scale(1.2);
-    box-shadow: 1px 1px 1px 1px rgba(244, 172, 148, 0.776)
-}
-li:active{
-    transform: scale(0.98)
-}
-.change{ 
-    display: absolute;  
-    float: right; 
-    border-radius: 15px; 
-    color: black; 
-    margin-left: 10vw; 
-    cursor: pointer; 
-    padding: 0.8vw;  
-}
-.change:hover{
-    font-weight: bold;
-    background-color: grey;
-    border-radius: 1.5vw;
-     
-   
-}
-@media screen and (max-width: 768px){
-.height{
-        height: 6vw; 
-        display: flex;
-        justify-content: flex-end;
-        width: 100%; 
-}
-ul{
+    justify-content: flex-end;
+    width: 100%;
+  }
+  ul {
     display: flex;
     flex-direction: column;
     padding: 0vw;
-    width: 100%; 
-
-}
-li{
+    width: 100%;
+  }
+  li {
     background-color: grey;
-    border-radius: 0px; 
-    height: 6vw;  
-    padding-top: 2vw; 
-}
-li:hover{
+    border-radius: 0px;
+    height: 6vw;
+    padding-top: 2vw;
+  }
+  li:hover {
     background-color: orangered;
     border-radius: 0px;
     transform: scale(1);
-    box-shadow: 0px 0px 0px 0px rgb(249, 248, 248)
-     
-}
-li:active{
-    transform: scale(0.98)
-}
-.fa-bars{
-    display: block; 
-    width: 5vw; 
+    box-shadow: 0px 0px 0px 0px rgb(249, 248, 248);
+  }
+  li:active {
+    transform: scale(0.98);
+  }
+  .fa-bars {
+    display: block;
+    width: 5vw;
     height: 5vw;
     color: black;
-    float: right; 
-    margin-top: 3vw; 
+    float: right;
+    margin-top: 3vw;
     margin-bottom: 5vw;
-    padding-right: 1.5vw;   
-}
-.fa-bars:hover{
+    padding-right: 1.5vw;
+  }
+  .fa-bars:hover {
     color: orangered;
-    
-}
-.fa-bars:active{
-    transform: scale(1.15)
-}
+  }
+  .fa-bars:active {
+    transform: scale(1.15);
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,159 +1,153 @@
 <template>
   <div>
-<div v-for="product in products" :key="product.id" class="grid">
-    <div class="image"><img class="skor-img" :src="product.img"></div>
-    <div class="beskrivning"><strong class="text">Beskrivning</strong><br><br><p class="text2">{{ product.beskrivning }}</p></div>
-    <div class="pris">Köp här<button @click="count++">{{ product.pris }} {{ count }}</button> </div>
-</div>
-</div>
+    <div v-for="product in products" :key="product.id" class="grid">
+      <div class="image"><img class="skor-img" :src="product.img" /></div>
+      <div class="beskrivning">
+        <strong class="text">Beskrivning</strong><br /><br />
+        <p class="text2">{{ product.beskrivning }}</p>
+      </div>
+      <div class="pris">
+        Köp här<button @click="count++">{{ product.pris }} {{ count }}</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup> 
-import p from "../data/products.json"
-import {ref} from 'vue'
+<script setup>
+import p from '../data/products.json'
+import { ref } from 'vue'
 
 const props = defineProps(['count'])
 let products = ref(p)
 const count = ref(0)
 
-
-  defineProps:({
-    count: Number
-  })
-
+defineProps: ({
+  count: Number,
+})
 </script>
 <style scoped>
-img{
+img {
   width: 7vw;
-  border-radius: 50%; 
+  border-radius: 50%;
   margin-top: 2vw;
-  margin-left: 2vw; 
-  transition-duration: 0.5s  
+  margin-left: 2vw;
+  transition-duration: 0.5s;
 }
-img:hover{
-  transform: rotate(360deg)
+img:hover {
+  transform: rotate(360deg);
 }
-.grid{
-  display: grid; 
-  grid-template-columns: auto auto auto; 
-  gap: 0.5vw;  
-  margin-top: 1vw; 
-  
+.grid {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  gap: 0.5vw;
+  margin-top: 1vw;
 }
-.image{
-  position: relative; 
-  grid-column: 1/ span 2;
+.image {
+  position: relative;
+  grid-column: 1 / span 2;
   grid-row: 1 / 3;
-  width: 50vw; 
-  height: 30.5vw; 
+  width: 50vw;
+  height: 30.5vw;
   margin-left: 0.7vw;
-  background-color: rgba(255, 255, 255, 0.708); 
-  border-radius: 0.8vw; 
+  background-color: rgba(255, 255, 255, 0.708);
+  border-radius: 0.8vw;
 }
-.beskrivning{
+.beskrivning {
   grid-column: 3;
-  grid-row: 2 ;
-  width: 26vw; 
-  height: 20vw; 
-  padding: 1vw; 
+  grid-row: 2;
+  width: 26vw;
+  height: 20vw;
+  padding: 1vw;
   background-color: rgba(255, 255, 255, 0.708);
   border-radius: 0.8vw;
-  padding-bottom: 2vw; 
-  margin-right: 0.5vw; 
+  padding-bottom: 2vw;
+  margin-right: 0.5vw;
 }
-.pris{
-  width: 26vw; 
-  height: 5vw; 
-  padding: 1vw; 
+.pris {
+  width: 26vw;
+  height: 5vw;
+  padding: 1vw;
   background-color: rgba(255, 255, 255, 0.708);
   border-radius: 0.8vw;
-  
 }
-button{
-  width: 8vw; 
+button {
+  width: 8vw;
   border-radius: 0.7vw;
-  border: none; 
-  padding: 0.5vw; 
-  background-color: orangered; 
-  float: right; 
-  margin-top: 3vw; 
-  margin-right: 10vw; 
-  cursor: pointer; 
-  font-weight: bold;  
+  border: none;
+  padding: 0.5vw;
+  background-color: orangered;
+  float: right;
+  margin-top: 3vw;
+  margin-right: 10vw;
+  cursor: pointer;
+  font-weight: bold;
 }
-button:hover{
-  background-color: rgb(241, 168, 141); 
+button:hover {
+  background-color: rgb(241, 168, 141);
 }
-button:active{
-  transform: scale(0.9)
+button:active {
+  transform: scale(0.9);
 }
-.skor-img{
-  position: absolute; 
-  width: 25vw; 
-  margin-left: 10vw; 
-}
-.text{
-  font-size: 2vw; 
-}
-.text2{
-  font-size: 1vw; 
-  
-}
-@media screen and (max-width: 768px){
-
-
-.hidden{
-  display: none; 
-}
-.grid{
-   
-  display: flex;
-  flex-direction: column; 
-  justify-content: center; 
-  
-   
-}
-
-.image{
-  margin-top: 2vw; 
-  position: static;
-  width: 78vw; 
-  margin-right: 1vw; 
-}
-.skor-img{
-  margin-left: 25vw; 
-}
-.beskrivning{
-  display: flex;
-  width: 76vw;
-  flex-direction: column;  
-  margin: 1vw; 
-  height: 80vw; 
-}
-.pris{
-  position: relative; 
-  display: flex; 
-  width: 76.5vw;
-  flex-direction: column; 
-  padding-bottom: 7.5vw;
-  margin: 0.6vw;
-  margin-bottom: 1vw; 
-  text-align: center;
-  font-size: 3vw;   
-}
-button{
+.skor-img {
   position: absolute;
-  width:76.5vw;
-  height: 7vw;  
-  margin-right: 0.5vw; 
-  margin-top: 4vw;
+  width: 25vw;
+  margin-left: 10vw;
 }
-.text{
-  font-size: 2.5vw;
-  
+.text {
+  font-size: 2vw;
 }
-.text2{
-  font-size: 2.5vw; 
+.text2 {
+  font-size: 1vw;
 }
+@media screen and (max-width: 768px) {
+  .hidden {
+    display: none;
+  }
+  .grid {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .image {
+    margin-top: 2vw;
+    position: static;
+    width: 78vw;
+    margin-right: 1vw;
+  }
+  .skor-img {
+    margin-left: 25vw;
+  }
+  .beskrivning {
+    display: flex;
+    width: 76vw;
+    flex-direction: column;
+    margin: 1vw;
+    height: 80vw;
+  }
+  .pris {
+    position: relative;
+    display: flex;
+    width: 76.5vw;
+    flex-direction: column;
+    padding-bottom: 7.5vw;
+    margin: 0.6vw;
+    margin-bottom: 1vw;
+    text-align: center;
+    font-size: 3vw;
+  }
+  button {
+    position: absolute;
+    width: 76.5vw;
+    height: 7vw;
+    margin-right: 0.5vw;
+    margin-top: 4vw;
+  }
+  .text {
+    font-size: 2.5vw;
+  }
+  .text2 {
+    font-size: 2.5vw;
+  }
 }
 </style>
