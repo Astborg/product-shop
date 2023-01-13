3e35bb173cf0f93956327006dfe4a8aa1fd05b65
Astborg <josefinastborg@hotmail.com>
2023-01-13 19:45:47 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="square">
+        <AppHeader/>
+        <nuxt/>
+    </div>
+</template>
+
+<script>
+  import AppHeader from '../components/AppHeader.vue'
+  
+  
+  export default{
+    components: {
+        AppHeader,
+        
+    }
+  }
+  </script>
+<style>
+* {
+    font-family:Arial, Helvetica, sans-serif
+}
+.square{
+    background-color: rgb(245, 243, 243);
+    width: 80vw;
+    height: 100%; 
+    margin: auto;
+    padding-bottom: 2vw;  
+}
+</style>
